2633bcb2a8ec7c2ec16f47b99f6a485e68d3b7db
TheEnmanuel23 <enmisac@gmail.com>
2023-08-06 21:36:04 -0600

    enable optomistic ui to like feature

--- app/likes.tsx
+++ app/likes.tsx
@@ -3,7 +3,13 @@
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useRouter } from "next/navigation";
 
-export default function Likes({ tweet }: { tweet: TweetWithAuthor }) {
+export default function Likes({
+  tweet,
+  addOptimisticTweet,
+}: {
+  tweet: TweetWithAuthor;
+  addOptimisticTweet: (newTweet: TweetWithAuthor) => void;
+}) {
   const router = useRouter();
 
   const handleLikes = async () => {
@@ -13,12 +19,24 @@ export default function Likes({ tweet }: { tweet: TweetWithAuthor }) {
     } = await supabase.auth.getUser();
 
     if (user) {
+      addOptimisticTweet({
+        ...tweet,
+        likes: tweet.likes - 1,
+        user_has_liked_tweet: !tweet.user_has_liked_tweet,
+      });
+
       if (tweet.user_has_liked_tweet) {
         await supabase
           .from("likes")
           .delete()
           .match({ user_id: user.id, tweet_id: tweet.id });
       } else {
+        addOptimisticTweet({
+          ...tweet,
+          likes: tweet.likes + 1,
+          user_has_liked_tweet: !tweet.user_has_liked_tweet,
+        });
+
         await supabase
           .from("likes")
           .insert({ user_id: user.id, tweet_id: tweet.id });
